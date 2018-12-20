ab2c3435edd67422460ae3756a1b08f25df9a065
Matthew Lang <matthew@matthewlang.net>
2018-12-20 00:04:03 +0000

    Add proportional tags based on count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def tag_cloud_css_class(tag_count)
+    total_count = TagCloud.all_tags_count.first['count']
+    percent = (tag_count.to_f / total_count.to_f) * 100
+
+    return 'tag-size-5' if percent > 10
+    return 'tag-size-4' if percent > 5
+    return 'tag-size-3' if percent > 2.5
+    return 'tag-size-2' if percent > 1.25
+    return 'tag-size-1' if percent > 0.625
+
+    'tag-size-0'
+  end
 end
--- app/models/tag_cloud.rb
+++ app/models/tag_cloud.rb
@@ -10,4 +10,15 @@ class TagCloud
       order by count DESC'
     @results = ActiveRecord::Base.connection.select_all(sql).to_hash
   end
+
+  def self.all_tags_count
+    sql = '
+      with tags as (
+        select unnest(tags) as tag_name from bookmarks
+      )
+      select count(*)
+      from tags
+      order by count DESC'
+    @results = ActiveRecord::Base.connection.select_all(sql).to_hash
+  end
 end
