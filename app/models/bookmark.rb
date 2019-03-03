9c98cda81dea4cf3849e6afbbaeecdcbedc83c1a
Matthew Lang <matthew@matthewlang.net>
2019-03-03 10:51:53 +0000

    Allow for tag delimination with commas and spaces (#21)
    Resolves #21

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -12,10 +12,6 @@ class Bookmark < ApplicationRecord
   validates :url, uniqueness: { case_sensitive: false }, presence: true
   validates :title, presence: true
 
-  def tags=(values)
-    super(values.split(' '))
-  end
-
   def tags_as_input
     tags.sort.join(', ')
   end
