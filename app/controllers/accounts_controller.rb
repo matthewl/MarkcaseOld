d60258cddc83a2c2db7b3a8bb69291563b1f2d2c
Matthew Lang <matthew@matthewlang.net>
2019-03-15 07:40:16 +0000

    Default behaviour for registrations

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AccountsController < ApplicationController
+	include SessionsHelper
+
   def new
     if Setting.first.single_account?
       redirect_to root_path
@@ -10,6 +12,13 @@ class AccountsController < ApplicationController
   end
 
   def create
+  	@account = Account.new()
+  	if @account.update_attributes(account_params)
+  		log_in @account
+  		redirect_to user_path(username: @account.login)
+  	else
+  		render :new
+  	end
 
   end
 
@@ -17,5 +26,11 @@ class AccountsController < ApplicationController
     account = current_account
     account.destroy
     redirect_to root_url, notice: t('account.destroyed')
+  end
+
+  private
+
+  def account_params
+		 params.require(:account).permit(:login, :email, :password)	
   end
 end
