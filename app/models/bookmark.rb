34fc1bb65b82d680c79f38e2cc08b167514e8241
Matthew Lang <matthew@matthewlang.net>
2019-03-26 22:56:49 +0000

    Same host bookmarks (#39)
    Adds related bookmarks by host.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -6,13 +6,22 @@ class Bookmark < ApplicationRecord
   scope :shared, -> { where(shared: true) }
   scope :counted_tags, -> { tag_counts_on(:tags).sort_by(&:taggings_count).reverse }
   scope :for_feeds, -> { where(shared: true).order('created_at DESC').limit(25) }
+  scope :by_host, ->(host, id) { where('url LIKE ?', "%#{host}%").order('created_at DESC').limit(5).where.not(id: id) }
 
   belongs_to :account
 
   validates :url, uniqueness: { case_sensitive: false }, presence: true
   validates :title, presence: true
 
+  def find_by_host
+    account.bookmarks.by_host(host, id)
+  end
+
   def tags_as_input
     tags.sort.join(', ')
+  end
+
+  def host
+    URI.parse(url).host
   end
 end
