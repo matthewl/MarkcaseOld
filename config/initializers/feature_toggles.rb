51925f1992097d5cf621e334cc65ee5a8db8cb99
Matthew Lang <matthew@matthewlang.net>
2019-04-21 17:33:02 +0100

    Add preview feature toggle to accounts

--- config/initializers/feature_toggles.rb
+++ config/initializers/feature_toggles.rb
@@ -4,4 +4,8 @@ FT = FeatureToggle.new.tap do |ft|
   ft.for(:accepting_registrations) do
     Account.count < 50
   end
+
+  ft.for(:importing_bookmarks) do |account_id:|
+    Account.where(id: account_id, preview_features: true).exists?
+  end
 end
