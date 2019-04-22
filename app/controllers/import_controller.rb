117ff94fc317ad3d0f2b180b369ced63160d2ac3
Matthew Lang <matthew@matthewlang.net>
2019-04-22 07:20:15 +0100

    File uploader working for import file

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -3,17 +3,19 @@
 class ImportController < AuthenticatedController
 
   def new
-    FT.with(:importing_bookmarks, account_id: current_user.id) do
-      render :new
+    FT.with(:importing_bookmarks, account_id: current_account.id) do
+      @account = current_account
+      @tags = find_tags_for_cloud(current_account)
+      return render :new
     end
 
-    redirect_to user_path(username: current_account.login)
+    redirect_to users_bookmarks_path
   end
   
   def create
     @account = current_account
     if @account.update_attributes(import_params)
-      redirect_to user_path(username: current_account.login)
+      redirect_to users_bookmarks_path
     else
       render :new
     end
@@ -21,7 +23,11 @@ class ImportController < AuthenticatedController
 
   private
 
-  def bookmark_params
+  def import_params
     params.require(:account).permit(:import_file)
+  end
+
+  def users_bookmarks_path
+    user_path(username: current_account.login)
   end
 end
